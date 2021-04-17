6df3315cee61a167ee1d0d17921ede0ee81c1806
BmeUP <44481957+BmeUP@users.noreply.github.com>
2021-04-17 15:33:21 +0300

    Update transfer.py
    Было - parser = ArgumentParser(description="Перевод денег между двемя пользователями")
    Стало - parser = ArgumentParser(description="Перевод денег между двумя пользователями")

--- src/transfer.py
+++ src/transfer.py
@@ -7,7 +7,7 @@ from connect import connect_production
 from model import MoneyAmountError
 from model.use_cases import transfer_by_name, get_user
 
-parser = ArgumentParser(description="Перевод денег между двемя пользователями")
+parser = ArgumentParser(description="Перевод денег между двумя пользователями")
 parser.add_argument("-f", "--from", dest="from_", type=str)
 parser.add_argument("-t", "--to", dest="to_", type=str)
 parser.add_argument("amount", type=Decimal)
